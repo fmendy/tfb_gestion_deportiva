be6602a6b57e9278444fa054906ca6221fc6c193
fmendy <alvaropaneda@gmail.com>
2026-06-09 09:44:11 +0200

    Fix location

---
+++ src/main/java/com/gestion/deportiva/DeportivaApplication.java
@@ -0,0 +1,14 @@
+package com.gestion.deportiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeportivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeportivaApplication.class, args);
+		/**/
+	}
+
+}
---
+++ src/test/java/com/gestion/deportiva/DeportivaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestion.deportiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeportivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
